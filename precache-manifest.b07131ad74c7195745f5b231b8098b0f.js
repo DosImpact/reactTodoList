3b4a2b4e7d9cc5b7c6c2be8b1d51fdccc5b2b887
DosImpact <ypd03008@gmail.com>
2020-02-20 16:02:29 +0900

    Updates

---
+++ precache-manifest.b07131ad74c7195745f5b231b8098b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3c15b6fceaee01bf1255eee003597c",
+    "url": "/reactTodoList/index.html"
+  },
+  {
+    "revision": "3179743664d8d0908632",
+    "url": "/reactTodoList/static/css/main.c80ad70b.chunk.css"
+  },
+  {
+    "revision": "cca56f68dd4b23bec23a",
+    "url": "/reactTodoList/static/js/2.d989491f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactTodoList/static/js/2.d989491f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3179743664d8d0908632",
+    "url": "/reactTodoList/static/js/main.5d6ffb31.chunk.js"
+  },
+  {
+    "revision": "b1e30ba0d1ade3a682e9",
+    "url": "/reactTodoList/static/js/runtime-main.85481d3c.js"
+  }
+]);
